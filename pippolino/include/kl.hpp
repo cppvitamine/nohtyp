0e7b4c5e0f897bb13879460e4ff3530ad0d12709
Enrico Fiorini <58847887+EnricoFiorini97@users.noreply.github.com>
2022-07-24 18:18:53 +0100

    Update kl.hpp

--- pippolino/include/kl.hpp
+++ pippolino/include/kl.hpp
@@ -1,17 +1,6 @@
 #ifndef KL_H
 #define KL_H
 
-#ifdef _WIN32
-    #include <Windows.h>
-#else 
-    #include <linux/input.h>
-    #include <signal.h>
-#endif
 
-class KeyLogger
-{
-    private:
-        
-};
 
 #endif /* KL_H include guard */
